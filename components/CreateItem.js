00d3da24ced62c01a8e17f146c640ca6cdec67b1
Zach VanSomeren <zvansomeren@gmail.com>
2018-10-31 09:42:56 -0700

    End of lesson 18.  Uploading images to Cloudinary.

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -35,10 +35,29 @@ class CreateItem extends Component {
     price: 0,
   };
 
-  handleChange = (e) => {
+  handleChange = e => {
     const { name, type, value } = e.target;
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: val });
+  }
+
+  uploadFile = async e => {
+    console.log('uploading file...');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/zvansom/image/upload/', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
   }
 
   render() {
@@ -59,6 +78,18 @@ class CreateItem extends Component {
           }}>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+                Image
+                <input 
+                  type="file" 
+                  id="file" 
+                  name="file" 
+                  placeholder="Upload an image" 
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img src={this.state.image} alt="Upload Preview" />}
+              </label>
+            
               <label htmlFor="title">
                 Title
                 <input 
