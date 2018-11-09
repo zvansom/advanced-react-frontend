0e1fd4d99417469d6c7c3107f72238431da18a5b
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-09 09:48:09 -0800

    End of lesson 42. Remove from cart component created.

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import formatMoney from '../lib/formatMoney';
 import styled from 'styled-components';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -29,6 +30,7 @@ const CartItem = ({ cartItem }) => (
         </em>
       </p>
     </div>
+    <RemoveFromCart id={cartItem.id} />
   </CartItemStyles>
 );
 
