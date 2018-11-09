e1ff1fad4baab68b8160869ccb2f81e90e971e36
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-08 16:18:23 -0800

    End of lesson 40. Add to Cart mutation connected to button

--- components/Item.js
+++ components/Item.js
@@ -6,6 +6,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from  './styles/PriceTag';
 import DeleteItem from './DeleteItem';
 import formatMoney from '../lib/formatMoney';
+import AddToCart from './AddToCart';
 
 export default class Item extends Component {
   static propTypes = {
@@ -36,7 +37,7 @@ export default class Item extends Component {
           }}>
             <a>Edit ✏️</a>
           </Link>
-          <button>Add to Cart</button>
+          <AddToCart id={item.id}/>
           <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
