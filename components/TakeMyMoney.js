f8add20e073b73d121f4aab2b9f7e8b6ec694a8f
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-12 15:15:27 -0800

    End of lesson 52. Order page query and display info about single order.

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -29,6 +29,7 @@ function totalItems(cart) {
 
 class TakeMyMoney extends React.Component {
   onToken = async (res, createOrder) => {
+    NProgress.start();
     console.log('On token called')
     console.log(res.id);
     // Call the mutation when we get the stripe token
@@ -39,7 +40,10 @@ class TakeMyMoney extends React.Component {
     }).catch(err => {
       alert(err.message);
     });
-    console.log(order);
+    Router.push({
+      pathname: '/order',
+      query: { id: order.data.createOrder.id },
+    })
   };
   
   render() {
