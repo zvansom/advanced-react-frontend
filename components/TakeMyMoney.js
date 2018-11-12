453d820831f32f2c4e6c4addcc2be3e5470dbea6
Zach VanSomeren <zvansomeren@gmail.com>
2018-11-12 14:40:59 -0800

    End of lesson 51. Corrected warnings in console.

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -28,17 +28,18 @@ function totalItems(cart) {
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = (res, createOrder) => {
+  onToken = async (res, createOrder) => {
     console.log('On token called')
     console.log(res.id);
     // Call the mutation when we get the stripe token
-    createOrder({
+    const order = await createOrder({
       variables: {
         token: res.id
       },
     }).catch(err => {
       alert(err.message);
     });
+    console.log(order);
   };
   
   render() {
@@ -54,7 +55,7 @@ class TakeMyMoney extends React.Component {
                 amount={calcTotalPrice(me.cart)}
                 name="Sick Fits"
                 description={`Order of ${totalItems(me.cart)} items.`}
-                image={me.cart[0].item && me.cart[0].item.image}
+                image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
                 stripeKey="pk_test_7sOY8qsDkeI2Mg8T7zoyuSVh"
                 currency="USD"
                 email={me.email}
